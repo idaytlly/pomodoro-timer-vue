5598aed52745ee6c3ea76dbd4269b67ef352958a
IDA YATULLAILIYEH BINTI AMRUN <idayatullailiyeh@graduate.utm.my>
2026-02-11 16:01:17 +0800

    deployment

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false
+  base: '/pomodoro-timer-vue/',
+  server: {
+    port: 3000,
+    open: true
   }
 })
