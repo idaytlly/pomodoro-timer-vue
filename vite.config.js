47696955119dd069af610faf7ff3f244e23ab163
IDA YATULLAILIYEH BINTI AMRUN <idayatullailiyeh@graduate.utm.my>
2026-02-11 21:55:41 +0800

    fix on vercel & github path

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [vue()],
-  base: '/pomodoro-timer-vue/',
-  server: {
-    port: 3000,
-    open: true
-  }
-})
+  base: mode === "github" ? "/pomodoro-timer-vue/" : "/",
+}))
